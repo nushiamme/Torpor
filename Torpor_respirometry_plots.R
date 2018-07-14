7923b3071130f97edd4ac31e5914a8d089361c9f
nushiamme <anusha.shankar@stonybrook.edu>
2018-07-14 17:45:01 +0200

    Small formatting changes for Ecology

--- Torpor_respirometry_plots.R
+++ Torpor_respirometry_plots.R
@@ -34,7 +34,7 @@ library(plyr) # only for 'revalue' function
 
 
 ## setwd and read in file
-setwd("C:\\Users\\shankar\\Dropbox\\Hummingbird energetics\\Feb2018\\Data")
+setwd("C:\\Users\\shankar\\Dropbox\\Hummingbird energetics\\July2018\\Data")
 
 ## Read in files
 torpor <- read.csv("Torpor_individual_summaries.csv") # Torpor summaries per individual
